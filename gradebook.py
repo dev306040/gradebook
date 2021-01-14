a52ea0438f344a334139eea75501a14ee0f405ff
Dev30 <dev306040@gmail.com>
2021-01-13 23:34:02 -0600

    first commit

---
+++ gradebook.py
@@ -0,0 +1,21 @@
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+
+
+subjects = ['physics', 'calculus', 'poetry', 'history']
+
+grades = [98, 97, 85, 88]
+
+subjects.append('computer science')
+grades.append(100)
+
+gradebook = zip(subjects,grades)
+
+subjects.append('visual arts')
+grades.append(93)
+
+full_gradebook = zip(gradebook, last_semester_gradebook)
+
+print(list(gradebook))
+
+
+
